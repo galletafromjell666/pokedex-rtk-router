c90ddbbe5fd7bd99d681704fe10115fce49232a8
galletafromjell666 <giococu666@gmail.com>
2023-01-16 19:50:25 -0600

    Fix animations

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useGetPokemonByNameQuery } from "../features/pokeApiSlice";
 import PokeBall from "./../assets/pokeball.png";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 const PreviewCard = ({ name }) => {
   const { data } = useGetPokemonByNameQuery(name);
@@ -11,56 +10,59 @@ const PreviewCard = ({ name }) => {
   }
 
   return (
-    <>
-      {data ? (
-        <section
-          style={styleBgColor}
-          className="relative max-w-[230px] h-[150px] rounded-xl overflow-hidden pt-3.5 pr-0 pb-2 pl-3"
-        >
-          <h1 className="capitalize font-bold tracking-tighter md:text-lg text-sm text-white">
-            {data.name}
-          </h1>
-          <h2 className="opacity-50 absolute right-[10px] top-[6px] font-bold tracking-wider text-[16px]">
-            #{String(`${data.id}`).padStart(3, "0")}
-          </h2>
-          <figure className="flex items-center justify-center">
-            <figcaption className="flex flex-col  justify-center items-center w-1/2">
-              {data.types.map((type, index) => (
-                <small
-                  key={index}
-                  className="typeName flex items-center content-center font-normal w-[75px]  capitalize text-xs my-1 rounded-full md:text-[0.95rem] "
-                >
-                  <span className="w-full px-[3px] py-[6px] text-center opacity-70">
-                    {type.type.name}
-                  </span>
-                </small>
-              ))}
-            </figcaption>
-            <div className="w-1/2 relative">
-              <img
-                className="absolute right-[-18px] bottom-[-10px] opacity-20"
-                src={PokeBall}
-                alt="Pokeball"
-              />
-              <div
-                className="relative w-full h-auto right-[2px] bottom-[12px]"
-                // src={img}
-                alt={data.name}
-              >
-                <LazyLoadImage
+    <div className="relative">
+      {!data && (
+        <div className="absolute z-50 w-full rounded-xl h-full is-loading"></div>
+      )}
+      <section
+        style={styleBgColor}
+        className="relative min-w-[200px] max-w-[230px] h-[150px] rounded-xl overflow-hidden pt-3.5 pr-0 pb-2 pl-3"
+      >
+        <h1 className="capitalize font-bold tracking-tighter md:text-lg text-sm text-white">
+          {data ? data.name : "loading"}
+        </h1>
+        <h2 className="opacity-50 absolute right-[10px] top-[6px] font-bold tracking-wider text-[16px]">
+          {data ? "#" + String(`${data.id}`).padStart(3, "0") : "#000"}
+        </h2>
+        <figure className="relative flex items-center justify-center">
+          <figcaption className="flex flex-col  justify-center items-center w-1/2">
+            {data
+              ? data.types.map((type, index) => (
+                  <small
+                    key={index}
+                    className="typeName flex items-center content-center font-normal w-[75px]  capitalize text-xs my-1 rounded-full md:text-[0.95rem] "
+                  >
+                    <span className="w-full px-[3px] py-[6px] text-center opacity-70">
+                      {type.type.name}
+                    </span>
+                  </small>
+                ))
+              : "Loading"}
+          </figcaption>
+          <div className="w-1/2 h-full relative">
+            {data && (
+              <div>
+                <img
+                  className="relative w-full z-50 h-auto right-[2px] bottom-[12px]"
                   src={data.sprites.other["official-artwork"].front_default}
-                  alt="Image Alt"
+                  alt={data.name}
                 />
               </div>
-            </div>
-          </figure>
-        </section>
-      ) : (
-        <div>
-          <h1>Loading</h1>
-        </div>
-      )}
-    </>
+            )}
+            {/* <LazyLoadImage
+                  
+                  alt="Image Alt"
+                  placeholderSrc={PokeBall}
+                /> */}
+          </div>
+        </figure>
+        <img
+          className="absolute w-[60%] right-[-40px] bottom-[-40px] z-10 opacity-20"
+          src={PokeBall}
+          alt="Pokeball"
+        />
+      </section>
+    </div>
   );
 };
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { LazyLoadComponent } from "react-lazy-load-image-component";
 import { useGetAllPokemonQuery } from "../features/pokeApiSlice";
 import { Link } from "react-router-dom";
 import PreviewCard from "../components/PreviewCard";
 import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
 import { incrementByAmount } from "../features/queryLimitSlice";
+import Loader from "../components/Loader";
 const index = () => {
+  const [showLoader, setShowLoader] = useState(true);
   const dispatch = useDispatch();
   const limitQuery = useSelector((state) => state.queryLimit.value);
   const { data: pokmData } = useGetAllPokemonQuery(limitQuery);
@@ -15,17 +18,33 @@ const index = () => {
     threshold: 1.0,
   });
   useEffect(() => {
+    if (pokmData) {
+      const timer = setTimeout(() => {
+        setShowLoader(false);
+      }, Math.random() * (500 - 900) + 500);
+      return () => clearTimeout(timer);
+    }
+  }, [pokmData]);
+
+  useEffect(() => {
     if (isVisible) dispatch(incrementByAmount(15));
   }, [isVisible]);
   return (
-    <div className="flex items-center justify-center bg-white min-h-max max-h-auto w-full">
+    <div className="relative flex  items-center justify-center bg-white min-h-max max-h-auto w-full">
+      {(showLoader || !pokmData) && (
+        <div className="absolute left-0 top-0 z-50 w-full h-screen bg-white">
+          <Loader />
+        </div>
+      )}
       {pokmData ? (
         <div className="relative grid gap-x-12 grid-cols-1 gap-y-12 2col:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 md:gap-x-28 ">
           {pokmData.results.map((poke, index) => {
             return (
               <div key={index} className="">
                 <Link to={`pokemon/${poke.name}`}>
-                  <PreviewCard {...poke} />
+                  <LazyLoadComponent threshold={250}>
+                    <PreviewCard {...poke} />
+                  </LazyLoadComponent>
                 </Link>
               </div>
             );
